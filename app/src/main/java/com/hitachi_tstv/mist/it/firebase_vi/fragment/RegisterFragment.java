0f180f295dcfd06b3ec6547687f1b5d42d0d9b53
evijung <evides01@gmail.com>
2017-11-25 15:02:18 +0700

    Back Controller Success

--- app/src/main/java/com/hitachi_tstv/mist/it/firebase_vi/fragment/RegisterFragment.java
+++ app/src/main/java/com/hitachi_tstv/mist/it/firebase_vi/fragment/RegisterFragment.java
@@ -6,7 +6,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toolbar;
 
+import com.hitachi_tstv.mist.it.firebase_vi.MainActivity;
 import com.hitachi_tstv.mist.it.firebase_vi.R;
 
 /**
@@ -14,6 +16,43 @@ import com.hitachi_tstv.mist.it.firebase_vi.R;
  */
 
 public class RegisterFragment extends Fragment {
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+
+        //Create Toolbar
+        createToolbar();
+
+
+    }//Main Method
+
+    private void createToolbar() {
+
+//        Config Toolbar
+       android.support.v7.widget.Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+
+        // Setup Title
+        ((MainActivity) getActivity()).getSupportActionBar().setTitle(getString(R.string.register));
+        ((MainActivity) getActivity()).getSupportActionBar().setTitle(getString(R.string.sub_register));
+
+//        Back Icon
+        ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                getActivity().getSupportFragmentManager().popBackStack();
+
+            }
+        });
+
+    }
 
     @Nullable
     @Override
