b3f1b4e5a1f41b72a18fe43516eb1f9489b4308b
evijung <evides01@gmail.com>
2017-11-25 11:58:30 +0700

    Design UI Main Success

--- app/src/main/java/com/hitachi_tstv/mist/it/firebase_vi/MainActivity.java
+++ app/src/main/java/com/hitachi_tstv/mist/it/firebase_vi/MainActivity.java
@@ -1,7 +1,10 @@
 package com.hitachi_tstv.mist.it.firebase_vi;
 
+import android.media.Ringtone;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.hitachi_tstv.mist.it.firebase_vi.fragment.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+//        Add Fragment
+        if (savedInstanceState == null) {
+
+//            Add Fragment to Activity
+            getSupportFragmentManager().beginTransaction().add(R.id.contentMainFragment, new MainFragment()).commit();
+        }
+
+    }//Main Method
+}// Main Class
---
+++ app/src/main/java/com/hitachi_tstv/mist/it/firebase_vi/fragment/MainFragment.java
@@ -0,0 +1,24 @@
+package com.hitachi_tstv.mist.it.firebase_vi.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.hitachi_tstv.mist.it.firebase_vi.R;
+
+/**
+ * Created by Tunyaporn on 11/25/2017.
+ */
+
+public class MainFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
+    }
+} //Main Class
