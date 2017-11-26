48c08e1a3f2fc2e55e5e8d92b243b32487fb3063
evijung <evides01@gmail.com>
2017-11-26 11:20:09 +0700

    Sign In Sucess

--- app/src/main/java/com/hitachi_tstv/mist/it/firebase_vi/fragment/MainFragment.java
+++ app/src/main/java/com/hitachi_tstv/mist/it/firebase_vi/fragment/MainFragment.java
@@ -1,21 +1,31 @@
 package com.hitachi_tstv.mist.it.firebase_vi.fragment;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.hitachi_tstv.mist.it.firebase_vi.R;
+import com.hitachi_tstv.mist.it.firebase_vi.utility.MyAlertDialog;
 
 /**
  * Created by Tunyaporn on 11/25/2017.
  */
 
 public class MainFragment extends Fragment{
-
+// Explicit
+    private  String emailString,passwordString;
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
@@ -23,8 +33,58 @@ public class MainFragment extends Fragment{
 //        Register Controller
         registerController();
 
+//        Login Controller
+        loginController();
 
     }// Main Method
+
+    private void loginController() {
+        Button button = getView().findViewById(R.id.btnLogin);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText emailEditText = getView().findViewById(R.id.edtEmail);
+                EditText passwordEditText = getView().findViewById(R.id.edtPassword);
+
+                // Get Value From EditText
+                emailString = emailEditText.getText().toString().trim();
+                passwordString = passwordEditText.getText().toString().trim();
+
+//                Check Space
+
+                if (emailString.isEmpty() || passwordString.isEmpty()) {
+                        // Have Space
+                    MyAlertDialog myAlertDialog = new MyAlertDialog(getActivity());
+                    myAlertDialog.myNormalDialog("Have Space",getString(R.string.sub_register));
+                } else {
+                    // No Space
+                    checkEmailAndPass();
+                }
+            }
+        });
+    }
+
+    private void checkEmailAndPass() {
+
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth.signInWithEmailAndPassword(emailString, passwordString).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+
+                if (task.isSuccessful()) {
+
+                    Toast.makeText(getActivity(),"Welcome",Toast.LENGTH_SHORT).show();
+                } else {
+
+                    MyAlertDialog myAlertDialog = new MyAlertDialog(getActivity());
+                    myAlertDialog.myNormalDialog("Authen False",task.getException().getMessage());
+                }
+            }
+        });
+
+
+    }
 
     private void registerController() {
         TextView textView = getView().findViewById(R.id.txtNewRegister);
